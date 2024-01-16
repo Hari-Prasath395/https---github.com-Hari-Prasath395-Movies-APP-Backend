04fe34fc617726f4d0801124175ff3762a4a9104
Hari-Prasath395 <hariiprasathh395@gmail.com>
2024-01-16 20:17:52 +0530

    changes updated

--- dbconfig.js
+++ dbconfig.js
@@ -3,9 +3,9 @@ const mongoose = require("mongoose");
 
 const connection = async()=>{
   await mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect("mongodb+srv://hariiprasathh:hariprasath@cluster0.j9p9nei.mongodb.net/BigMovies?retryWrites=true&w=majority")
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB Atlas");
   })
   .catch((err) => {
     console.log("Connection Error: ", err);
